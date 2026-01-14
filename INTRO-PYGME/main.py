377165b447d5a39b2a2f2c9e1133f6b11c910e66
Judith Darias <judithdariascurbelo98@gmail.com>
2026-01-14 21:49:10 +0100

    mostrar 100 figuras

--- INTRO-PYGME/main.py
+++ INTRO-PYGME/main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from figura_class import Figura
+import random as ra
 
 #inicializar todos los modulos de pygmae (pantallas,objetos,sonidos,etc)
 pg.init()
@@ -10,10 +11,13 @@ pantalla = pg.display.set_mode( (x_pos,y_pos))#definicion de tamaño de pantalla
 pg.display.set_caption("Intro Pygame")#agregar titulo en string a mi ventana
 
 game_over = True
-rectangulo1 = Figura(0,300,color=(201,92,56))
-rectangulo2 = Figura(20,500)
-rectangulo3 = Figura(0,400,color=(222,22,55))
-circulo1 = Figura(200,500,color=(131,214,71),radio=20)
+
+lista_circulos=[]
+lista_rectangulos= []
+#por cada vuelta del for voy guardando en lista un objeto figura con datos de entradas diferentes con el random.randint
+for i in range(100):
+    lista_circulos.append(Figura(ra.randint(0,800),ra.randint(0,600),color=(ra.randint(0,255),ra.randint(0,255),ra.randint(0,255)),radio=ra.randint(10,100)))
+    lista_rectangulos.append(Figura(ra.randint(0,800),ra.randint(0,600),color=(ra.randint(0,255),ra.randint(0,255),ra.randint(0,255)),w=ra.randint(10,100),h=ra.randint(10,100)))
 
 while game_over:
     for eventos in pg.event.get(): #capturar todos los eventos mientras se ejecuta el bucle
@@ -21,20 +25,25 @@ while game_over:
         if eventos.type == pg.QUIT:
             game_over = False
 
-
     pantalla.fill((74,208,245))#asignar el color de pantalla en rgb
     
-    rectangulo1.mover(x_pos,y_pos)
-    rectangulo2.mover(x_pos,y_pos)
-    circulo1.mover(x_pos,y_pos)
-        
-    #agregamos objeto a la pantalla
-    #draw.rect(sourface,color en (rgb),posiciones (posicionX,posicionY,tamañoX, tamañoY)
-    rectangulo1.dibujar_rectangulo(pantalla)
-    rectangulo2.dibujar_rectangulo(pantalla)
-    rectangulo3.dibujar_rectangulo(pantalla)
-    circulo1.dibujar_circulo(pantalla)
+    #recorremos la lista de circulos cargada y llamamos a los metodos de mover y dibujar para que muestre en pantalla
+    
+    """for circulo in lista_circulos:
+        circulo.mover(x_pos,y_pos)
+        circulo.dibujar_circulo(pantalla)
 
+    for rectangulo in lista_rectangulos:
+        rectangulo.mover(x_pos,y_pos)
+        rectangulo.dibujar_rectangulo(pantalla)"""
+
+    for i in range(100):
+        lista_circulos[i].mover(x_pos,y_pos)
+        lista_rectangulos[i].mover(x_pos,y_pos)
+        lista_circulos[i].dibujar_circulo(pantalla)
+        lista_rectangulos[i].dibujar_rectangulo(pantalla)
+        
+    
 
 
     pg.display.flip()#funcion para recargar toda la configuracion que va 

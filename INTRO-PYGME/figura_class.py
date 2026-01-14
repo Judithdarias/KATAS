71cb68363cad952b1dc5dbc38a33c922924688ab
Judith Darias <judithdariascurbelo98@gmail.com>
2026-01-14 20:35:33 +0100

    creacion 4º objeto, circulo

--- INTRO-PYGME/figura_class.py
+++ INTRO-PYGME/figura_class.py
@@ -1,5 +1,6 @@
 import pygame as pg
 class Rectangulo:
+
     def __init__(self,pos_x,pos_y,color=(255,255,255),w=10,h=30):
         self.pos_x=pos_x
         self.pos_y=pos_y
@@ -20,3 +21,23 @@ class Rectangulo:
 
     def dibujar(self,sourface):
         pg.draw.rect(sourface,self.color,(self.pos_x,self.pos_y,self.w,self.h))
+
+class Circulo:
+    def __init__(self,pos_x,pos_y,color=(255,255,255),radio=30):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.color = color
+        self.radio = radio 
+        self.vx = 1
+        self.vy = 1
+
+    def dibujar(self,pantalla):
+         pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
+
+    def mover(self,x_max,y_max):
+        self.pos_x = self.pos_x + self.vx
+        self.pos_y = self.pos_y + self.vy
+        if self.pos_x==0 or self.pos_x == x_max:
+            self.vx = self.vx*-1 
+        if self.pos_y==0 or self.pos_y == y_max:
+            self.vy = self.vy*-1 
--- INTRO-PYGME/main.py
+++ INTRO-PYGME/main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from figura_class import Rectangulo
+from figura_class import Rectangulo,Circulo
 
 #inicializar todos los modulos de pygmae (pantallas,objetos,sonidos,etc)
 pg.init()
@@ -12,6 +12,8 @@ pg.display.set_caption("Intro Pygame")#agregar titulo en string a mi ventana
 game_over = True
 rectangulo1 = Rectangulo(0,300,(201,92,56))
 rectangulo2 = Rectangulo(20,500)
+rectangulo3 = Rectangulo(0,400,(222,22,55))
+circulo1 = Circulo(200,500,(131,214,71),20)
 
 while game_over:
     for eventos in pg.event.get(): #capturar todos los eventos mientras se ejecuta el bucle
@@ -20,27 +22,23 @@ while game_over:
             game_over = False
 
 
-    pantalla.fill((81,189,245))#asignar el color de pantalla en rgb
+    pantalla.fill((74,208,245))#asignar el color de pantalla en rgb
     
     rectangulo1.mover(x_pos,y_pos)
-    rectangulo1.mover(x_pos,y_pos)
-
-    """
-    x = x + vx
-    y = y + vy
-    if x==0 or x == 800:
-        vx = vx*-1 
-    if y==0 or y == 600:
-        vy = vy*-1 
-        """
+    rectangulo2.mover(x_pos,y_pos)
+    circulo1.mover(x_pos,y_pos)
         
     #agregamos objeto a la pantalla
     #draw.rect(sourface,color en (rgb),posiciones (posicionX,posicionY,tamañoX, tamañoY)
     rectangulo1.dibujar(pantalla)
     rectangulo2.dibujar(pantalla)
+    rectangulo3.dibujar(pantalla)
+    circulo1.dibujar(pantalla)
+
 
 
     pg.display.flip()#funcion para recargar toda la configuracion que va 
+
 
 #cierre de pantalla
 pg.quit()

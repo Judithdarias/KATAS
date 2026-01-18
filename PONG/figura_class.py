6c3b6262a9d2d6c158a662059bc7000a589f2223
Judith Darias <judithdariascurbelo98@gmail.com>
2026-01-18 19:36:55 +0100

    raya discontinua

--- PONG/figura_class.py
+++ PONG/figura_class.py
@@ -11,22 +11,46 @@ class Raqueta:
     def dibujar(self,screen):
         pg.draw.rect(screen,self.color,(self.pos_x-(self.w),(self.pos_y-self.h//2),self.w,self.h))
 
+    def mover(self,teclado_arriba,teclado_abajo):
+        teclado = pg.key.get_pressed()
+    
+        if teclado[teclado_arriba] ==True and self.pos_y >=0+(self.h//2):
+            self.pos_y = self.pos_y -1
+        if teclado[teclado_abajo]==True and self.pos_y <=600-(self.h//2):
+            self.pos_y = self.pos_y +1
+
 class Pelota:
     def __init__(self,posx,posy,color=(255,255,255),radio=20):
         self.pos_x = posx
         self.pos_y = posy
         self.color = color
         self.radio = radio    
+        self.vx = 1
+        self.vy = 1
+        self.contadorDerecho = 0
+        self.contadorIzquierdo = 0
 
 
     def dibujar(self,screen):
         pg.draw.circle(screen,self.color,(self.pos_x,self.pos_y),self.radio)
 
-class Linea:
-    def __init__(self,posx,posy,width):
-        self.pos_x = posx
-        self.pos_y = posy
-        self.width = width 
-    
+    def mover(self,xmax,ymax):
+        self.pos_x = self.pos_x + self.vx
+        self.pos_y = self.pos_y - self.vy
+        
+        #limite derecho
+        if self.pos_x >= xmax+(8*self.radio):
+            self.vx = self.vx*-1
+            self.contadorIzquierdo +=1
+
+        #limite izquierdo
+        if self.pos_x <=0-(8*self.radio):
+            self.vx = self.vx*-1
+            self.contadorDerecho +=1
+
+        if self.pos_y >= ymax-(self.radio) or self.pos_y <=0+(self.radio):
+            self.vy = self.vy*-1    
+
+
     
 
--- PONG/main.py
+++ PONG/main.py
@@ -9,19 +9,44 @@ raqueta1 = Raqueta(10,300)
 raqueta2 = Raqueta(800,300)
 pelota = Pelota(400,300,color =(225,232,70))
 
+#definir tiempo de tasa de refresco dentro del bucle que fotogramas fps=fotograma por segundo
+tasa_refresco= pg.time.Clock()
+
 game_over = True
 while game_over:
+       #obtenemos la tasa de refresco en milisegundos
+    valor_tasa = tasa_refresco.tick(300)#variable para controlar la velocidad entre fotogramas
+    #print(valor_tasa)
     for eventos in pg.event.get():
         if eventos.type == pg.QUIT:
-            game_over=False
+            game_over = False
+  
+    pelota.mover(800,600)    
+    
     pantalla_principal.fill((53,104,45))
 
+    """
     pg.draw.line(pantalla_principal,(255,255,255),(400,0),(400,600),width=10)
+    """
+    
+    x = 400
+    raya = 15   
+    espacio = 10   
 
+    y = 0
+    while y < 600:
+        pg.draw.line(pantalla_principal,(255, 255, 255),(x, y),(x, y + raya),width=10)
+        y += raya + espacio
+
+    
     raqueta1.dibujar(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
     pelota.dibujar(pantalla_principal)
     
+    raqueta1.mover(pg.K_w,pg.K_s)
+    raqueta2.mover(pg.K_UP,pg.K_DOWN)
+    print("raqueta1:",pelota.contadorDerecho)
+    print("raqueta2:",pelota.contadorIzquierdo)
     pg.display.flip()
 
 

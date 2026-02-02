fa2cbdb5fdadadcc85bd7d8eeca3bd10d4b60766
Judith Darias <judithdariascurbelo98@gmail.com>
2026-02-02 20:40:36 +0100

    prueba con parametros

--- FLASK_INTRO/main.py
+++ FLASK_INTRO/main.py
@@ -13,6 +13,33 @@ app = Flask(__name__)
 #comando para ejecutar el servidor
 #flask --app main run
 
+#comando para ejecutar el servidor en otro puerto diferente al de default que es el 5000
+#flask --app main run -p 5002
+
+#comando para activar el servidor de flask en modo debug, no parar el servidor para ver los cambios
+#flask --app main --debug run
+
 @app.route('/hola')
 def hola_mundo():
-    return "Hola mundo flask"
+    return "Esto es una intro a flask, creo que mola, metira mola mucho"
+
+@app.route('/frutas')
+def list_frutas():
+    list_fruta = ['platano','fresa','piña','uva','melon']
+    return list_fruta
+
+@app.route('/diccionario')
+def list_dic():
+    dic = [{'name':'Maria','email':'maria@email.com'},{'name':'Carlos','email':'carlos@email.com'}]
+    return dic
+
+#pasar parametro por ruta url
+@app.route("/nombre/<name>")
+def tu_nombre(name):
+    print("valor: ",type(name))
+    return f"hola {name} como estas"
+
+@app.route("/num/<int:parametro>")
+def cuadrado(parametro):
+    #parametro = int(parametro)
+    return f"El cuadrado de {parametro} es {parametro*parametro}"
--- FLASK_INTRO/readme.md
+++ FLASK_INTRO/readme.md
@@ -11,3 +11,30 @@ pip install -r requirements.txt
 ```
 
 la libreria utilizada es https://flask.palletsprojects.com
+
+
+# Ejecución del programa
+
+-inicializar parametros para el servidor de flask
+-en mac:
+```
+export FLASK_APP=main.py
+```
+-en windows:
+```
+set FLASK_APP=main.py
+```
+-comando para ejecutar el servidor
+```
+flask --app main run
+```
+
+-comando para ejecutar el servidor en otro puerto diferente al de default que es el 5000
+```
+flask --app main run -p 5002
+```
+
+-comando para activar el servidor de flask en modo debug, no parar el servidor para ver los cambios
+```
+flask --app main --debug run
+```

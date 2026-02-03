317cc78872e5e4a3963f9e3af13c72a2344be39a
Judith Darias <judithdariascurbelo98@gmail.com>
2026-02-03 19:35:51 +0100

    operaciones

--- FLASK_INTRO/main.py
+++ FLASK_INTRO/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 
 #inicializar la varaible app con clask
@@ -43,3 +43,50 @@ def tu_nombre(name):
 def cuadrado(parametro):
     #parametro = int(parametro)
     return f"El cuadrado de {parametro} es {parametro*parametro}"
+
+#realizar una ruta que dinamicamente pueda solicitar o realizar
+#operaciones matematicas de suma,resta,multiplicacion y division
+# debe ingresar dos parametros numericos y la operacion a realizar
+@app.route("/calculador/<int:num1>/<int:num2>/<op>")
+def operaciones_mates(num1,num2,op):#op suma,resta, multi,divi
+    result=""
+    if op == "suma":
+        result = f"La suma es: {num1+num2}"
+    elif op == "resta":
+        result = f"La resta es: {num1-num2}"
+    elif op == "multi":
+        result = f"La multiplicación es: {num1*num2}"
+    elif op == "divi":
+        result = f"La división es: {num1/num2}"
+
+    return result
+
+
+@app.route("/html")
+def mi_html():
+    return render_template('hola.html',variable = "Hola esto es una variable desde metodo",nombre="Rolando",list_fruta = ['platano','fresa','piña','uva','melon'])
+
+@app.route("/segunda")
+def mi_html_segunda():
+    return render_template('segunda.html')
+
+#ejercicio
+#al metodo operaciones_mates, agregamos una vista html y mostramos dentro de esta
+#su resultado y los numeros y la operacion ejemplo: 10 + 5 = 15,
+#si el usuario no pasa una operacion mostrar un mensaje 
+#que diga debe ingresar la operacion : suma,resta,multi,divi
+
+@app.route("/calculadora/<int:num1>/<int:num2>/<op>")
+def operaciones_mates_tarea(num1,num2,op):#op suma,resta, multi,divi
+    result=""
+    if op == "suma":
+        result = f"La suma es: {num1} + {num2} = {num1+num2}"
+    elif op == "resta":
+        result = f"La resta es: {num1} - {num2} ={num1-num2}"
+    elif op == "multi":
+        result = f"La multiplicación es: {num1} * {num2} = {num1*num2}"
+    elif op == "divi":
+        result = f"La división es: {num1} / {num2} = {num1/num2}"
+    else:
+        result = "debe ingresar la operacion : suma,resta,multi,divi"
+    return render_template('tarea.html', resultado = result)

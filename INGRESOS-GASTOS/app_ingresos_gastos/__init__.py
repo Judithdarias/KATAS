c4ea60411e2817a24a4a8f3c238a8336931c51ff
Judith Darias <judithdariascurbelo98@gmail.com>
2026-02-03 21:18:51 +0100

    nueva pagina

---
+++ INGRESOS-GASTOS/app_ingresos_gastos/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from app_ingresos_gastos.routes import *
+
---
+++ INGRESOS-GASTOS/main.py
@@ -0,0 +1,4 @@
+from app_ingresos_gastos import app
+
+#set FLASK_APP=main.py
+#flask --app main --debug run

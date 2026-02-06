46a93c8be78aca745874be5a91617c81ea4404c6
Judith Darias <judithdariascurbelo98@gmail.com>
2026-02-06 19:59:29 +0100

    validacion de fecha de formulario redireccion

--- INGRESOS-GASTOS/app_ingresos_gastos/routes.py
+++ INGRESOS-GASTOS/app_ingresos_gastos/routes.py
@@ -1,16 +1,19 @@
 from app_ingresos_gastos import app
-from flask import render_template
+from flask import render_template,request,redirect
 import csv
+from datetime import date
 
 #http://127.0.0.1:5000/
 @app.route("/")
 def index():
+    
     datos=[]
     fichero = open('app_ingresos_gastos/data/movimientos.csv','r')
     lectura = csv.reader(fichero,delimiter=",",quotechar='"')
     for item in lectura:
         datos.append(item) 
 
+    fichero.close()
     """
     datos = [
         {'fecha':'01/09/2025','concepto':'Salario','monto':1800},
@@ -22,9 +25,27 @@ def index():
 
 
 #http://127.0.0.1:5000/new
-@app.route("/new")
+@app.route("/new",methods=["GET","POST"])
 def new():
-    return render_template("new.html",title="Registro", titulo="Registro",boton="Guardar") 
+    if request.method == "POST":
+        fecha_actual = str(date.today())
+
+        if request.form['dfecha'] > fecha_actual:
+            return render_template("new.html",title="Registro", titulo="Registro",boton="Guardar")
+        else:
+            #acceder al archivo y configurar la carga del nuevo registro
+            mifichero = open('app_ingresos_gastos/data/movimientos.csv','a',newline="")
+            #llamar al metodo writer de escritura y configuramos el formato
+            lectura = csv.writer(mifichero,delimiter=",",quotechar='"')
+            #registramos los datos recibidos desde el formulario al archivo csv
+            lectura.writerow( [request.form['dfecha'],request.form['dconcepto'],request.form['dmonto']] )
+            #cierre del archivo moviemientos.csv
+            mifichero.close()
+
+            return redirect("/")#esto es para redirigir a la ruta home
+        
+    else:#esto seria GET
+        return render_template("new.html",title="Registro", titulo="Registro",boton="Guardar") 
 
 #http://127.0.0.1:5000/delete
 @app.route("/delete")
--- INGRESOS-GASTOS/prueba.py
+++ INGRESOS-GASTOS/prueba.py
@@ -10,9 +10,30 @@ resultado = open('app_ingresos_gastos/data/movimientos.csv','r')
 lectura = resultado.readlines()
 print(lectura)
 """
+"""
 import csv
 
 mifichero = open('app_ingresos_gastos/data/movimientos.csv','r')
 lectura = csv.reader(mifichero,delimiter=",",quotechar='"')
 for items in lectura:
     print(items)
+
+mifichero.close()    
+"""
+"""
+import csv
+
+mifichero = open('app_ingresos_gastos/data/movimientos.csv','a',newline="")
+lectura = csv.writer(mifichero,delimiter=",",quotechar='"')
+lectura.writerow(['05/02/2026','merienda','-16'])
+
+mifichero.close()
+"""
+from datetime import date
+
+print(str(date.today()))
+
+if str(date.today()) < '2026-02-06':
+    print("es verdadero")
+else:
+    print("es falso")

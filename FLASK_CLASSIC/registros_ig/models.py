ee7d58c1e65ad9634a4ad46cf3d6c05979f3bed0
Judith Darias <judithdariascurbelo98@gmail.com>
2026-02-13 19:12:55 +0100

    Diseño

--- FLASK_CLASSIC/registros_ig/models.py
+++ FLASK_CLASSIC/registros_ig/models.py
@@ -1,28 +1,22 @@
-import sqlite3 
+import sqlite3
+
 
 def select_all():
-    """
-    diccionario=[
-        {"fecha":"2026-02-01","concepto":"Nomina","monto":2800},
-        {"fecha":"2026-02-02","concepto":"mercado","monto":-100},
-        {"fecha":"2026-02-05","concepto":"Desayuno","monto":-12},
-        {"fecha":"2026-02-06","concepto":"Almuerzo","monto":-25},
-    ]
-"""
     conexion = sqlite3.connect("data/movimientos.sqlite")
     cur = conexion.cursor()
     res = cur.execute("SELECT * FROM movements;")
-    filas = res.fetchall()#datos de columnas 
+    filas = res.fetchall()#datos de filas
     columnas = res.description#las columnas
 
     lista_diccionario=[]
     for f in filas:
-        posicion = 0 
+        posicion = 0
         diccionario = {}
         for c in columnas:
             diccionario[c[0]] = f[posicion]
-            posicion += 1
-        
-        lista_diccionario.append(diccionario)
+            posicion +=1
+        lista_diccionario.append(diccionario)   
+
+    conexion.close()
 
     return lista_diccionario
--- FLASK_CLASSIC/registros_ig/routes.py
+++ FLASK_CLASSIC/registros_ig/routes.py
@@ -1,9 +1,17 @@
 from registros_ig import app
-from flask import render_template
+from flask import render_template,request
 from registros_ig.models import *
 
 @app.route("/")
 def index():
     registros = select_all()
     return render_template("index.html",datos = registros)
+
+
+@app.route("/new",methods=['GET','POST'])
+def create():
+    if request.method == "GET":
+        return render_template("create.html")
+    else:
+        return f"aqui debo guardar en base de datos el registro: {request.form}"
 
